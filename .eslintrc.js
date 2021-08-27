4d18a2ee08e02abd680fe8f833eeff26ac9913f8
caral <cxs4754@rit.edu>
2021-08-27 14:30:31 -0400

    Add lint and formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-use-before-define': 'off',
+    'prettier/prettier': 'error',
+  },
 };
