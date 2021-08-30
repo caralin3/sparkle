b4eccf7b7c2dc934debb98136d1b560c65b0017a
Cara Steinberg <caralin3@gmail.com>
2021-08-30 10:53:46 -0400

    Add storybook

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { HomeStack } from './HomeStack';
+import { StoriesScreen } from '../screens';
 
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigator: React.FC = () => (
   <Drawer.Navigator drawerPostition="right">
     <Drawer.Screen name="Home" component={HomeStack} />
+    <Drawer.Screen name="Storybook" component={StoriesScreen} />
   </Drawer.Navigator>
 );
---
+++ src/screens/StoriesScreen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StorybookUIRoot from '../../storybook';
+
+export const StoriesScreen: React.FC = () => {
+  return <StorybookUIRoot />;
+};
